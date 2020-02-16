98cec428d5e9d8be9e13775338a7bfa3b830285c
NuiS4ncE <nuis4nce@hotmail.com>
2020-02-16 17:23:08 +0200

    4.3 done

--- utils/middleware.js
+++ utils/middleware.js
@@ -17,7 +17,7 @@ const errorHandler = (error, req, res, next) => {
 
     if (error.name === 'CastError' && error.kind === 'ObjectId') {
         return res.status(400).send({ error: 'malformatted id' })
-    } else if (error.name == 'Validation Error') {
+    } else if (error.name === 'Validation Error') {
         return res.status(400).json({ error: error.message })
     }
 
