ebaa5696d25ef969dda6a028c022c4cdf596f101
NuiS4ncE <nuis4nce@hotmail.com>
2020-02-16 17:42:53 +0200

    4.4 done

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,14 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const num = blogs.reduce((a, b) => {
+        return a + b.likes
+    }, 0)
+    return num
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
 }
